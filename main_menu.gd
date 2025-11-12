b58a505407433f969f364250082d502825fb9c70
Gabriel Bessi <gabriel.bessi.gb@gmail.com>
2025-11-12 00:31:51 -0300

    addicionar a sprite da esteira e fazer animação

--- main_menu.gd
+++ main_menu.gd
@@ -12,7 +12,7 @@ func _process(delta: float) -> void:
 
 
 func _on_iniciar_pressed() -> void:
-	get_tree().change_scene_to_file("res://node_2d.tscn")
+	get_tree().change_scene_to_file("res://partida.tscn")
 
 
 func _on_loja_pressed() -> void:
