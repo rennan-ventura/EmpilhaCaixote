4d5c4f8d7dbd01b9c4532f21cffc0c45089f7bcf
Gabriel Bessi <gabriel.bessi.gb@gmail.com>
2025-11-13 01:56:03 -0300

    add assets e pagina de partida

--- main_menu.gd
+++ main_menu.gd
@@ -6,11 +6,6 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func _on_iniciar_pressed() -> void:
 	get_tree().change_scene_to_file("res://partida.tscn")
 
